205fd4cd44f9ee4f94770fd5a79bdbb286ad8e18
Amrithesh Kakkoth <amritheshkakkoth10@gmail.com>
2025-09-10 14:58:33 +0530

    Create jtest.js

---
+++ jtest.js
@@ -0,0 +1,19 @@
+const str = "table football";
+
+const regex = /fo+/;
+const globalRegex = /fo+/g;
+
+console.log(regex.test(str));
+// Expected output: true
+
+console.log(globalRegex.lastIndex);
+// Expected output: 0
+
+console.log(globalRegex.test(str));
+// Expected output: true
+
+console.log(globalRegex.lastIndex);
+// Expected output: 9
+
+console.log(globalRegex.test(str));
+// Expected output: false
